036339c1508094dc24adf7406f6b3f9f04eaecc3
Prof OKalu <okalu1@outlook.com>
2025-04-01 14:33:48 -0500

    Updated products

--- src/main/java/edu/miu/cs/cs489/productmgmtapp/ProductMgmtApp.java
+++ src/main/java/edu/miu/cs/cs489/productmgmtapp/ProductMgmtApp.java
@@ -10,7 +10,8 @@ public class ProductMgmtApp {
     public static void main(String[] args) {
         var products = List.of(
                 new Product(101, "Banana", LocalDate.of(2025,3,31)),
-                new Product(102, "Apple", LocalDate.of(2025,4,1))
+                new Product(102, "Apple", LocalDate.of(2025,4,1)),
+                new Product(103, "CArrot", LocalDate.of(2025,4,1))
         );
         System.out.println(products);
     }
